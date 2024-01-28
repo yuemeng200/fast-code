1b1536424150006c3e0abc19da4738393168f898
shichangzhen <yuemeng200@outlook.com>
2024-01-28 20:19:42 +0800

    feature: enter sub components

---
+++ NOTE/index.md
@@ -0,0 +1,11 @@
+# NOTE
+
+## Question
+
+- debug 时如何指定工作目录
+- 如何通过 Event 的方式监听案件被按下，而不是注册快捷键的方式
+- 如何获取鼠标悬停的内容而不是 focus
+
+## Tip
+
+&& editorLangId == 'vue'
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,75 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+import * as path from 'path'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fast-code" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fast-code.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from fast-code!');
-	});
-
-	context.subscriptions.push(disposable);
+function log(text: string) {
+  if (process.env.NODE_ENV === 'development') {
+    vscode.window.showInformationMessage(text)
+  }
 }
 
-// This method is called when your extension is deactivated
+// 将 kebab-case 转换为 PascalCase
+function formatComponentName(componentName: string) {
+  return componentName.replace(/-(\w)/g, function (_, letter) {
+    return letter.toUpperCase()
+  })
+}
+
+function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+// 从文本中查找组件 import 的相对路径
+function findImportRelativePath(fileText: string, componentName: string) {
+  // fix regex to support the uppercase componentName both
+  const regex = `import\\s+(?:${componentName}|${capitalize(componentName)})\\s+from\\s+['"](.*)['"]`;
+  const match = fileText.match(regex)
+  if (match) {
+    return match[1]
+  }
+  return ''
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  let command = vscode.commands.registerCommand(
+    'fast-code.helloWorld',
+    async () => {
+      let editor = vscode.window.activeTextEditor
+      if (!editor) {
+        vscode.window.showErrorMessage('No active text editor')
+        return
+      }
+      const activeDocument = editor.document
+      const lineNumber = editor.selection.active.line
+      const lineText = activeDocument.lineAt(lineNumber).text
+      const tagNameRegex = /<([^>]+)>/
+      const tagNameMatch = lineText.match(tagNameRegex)
+      if (!tagNameMatch) {
+        log('No match found')
+        return
+      }
+      const componentName = formatComponentName(tagNameMatch[1])
+
+      const fileText = activeDocument.getText()
+      const RelativePath = findImportRelativePath(fileText, componentName)
+      log(RelativePath)
+      if (RelativePath) {
+        const activeDocumentPath = activeDocument.uri.fsPath
+        const targetPath = path.resolve(
+          path.dirname(activeDocumentPath),
+          RelativePath
+        )
+        const targetDocument = await vscode.workspace.openTextDocument(
+          targetPath
+        )
+        await vscode.window.showTextDocument(targetDocument)
+      } else {
+        log('Component not found')
+      }
+    }
+  )
+
+  context.subscriptions.push(command)
+}
+
 export function deactivate() {}
