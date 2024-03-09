eeb6ce1477d567c854c8e7c54928d243afc25dd4
shichangzhen <yuemeng200@outlook.com>
2024-03-09 17:13:52 +0800

    chore

--- src/services/tools/commands/convert-color.ts
+++ src/services/tools/commands/convert-color.ts
@@ -1,42 +1,5 @@
 import * as vscode from 'vscode'
-
-/**
- *  Convert rgb to hex
- * @param r
- * @param g
- * @param b
- * @returns
- */
-function rgbToHex(r: number, g: number, b: number): string {
-  const rHex = r.toString(16)
-  const gHex = g.toString(16)
-  const bHex = b.toString(16)
-  return rHex.length === 1 && gHex.length === 1 && bHex.length === 1
-    ? `#${rHex}${gHex}${bHex}`
-    : `#${rHex.padStart(2, '0')}${gHex.padStart(2, '0')}${bHex.padStart(
-        2,
-        '0'
-      )}`
-}
-
-/**
- *  Convert hex to rgb
- * @param str
- * @returns
- */
-function hexToRgb(str: string): string {
-  let hex = str.replace('#', '')
-  if (hex.length === 3) {
-    hex = hex
-      .split('')
-      .map(char => char + char)
-      .join('')
-  }
-  const r = parseInt(hex.substring(0, 2), 16)
-  const g = parseInt(hex.substring(2, 4), 16)
-  const b = parseInt(hex.substring(4, 6), 16)
-  return `rgb(${r}, ${g}, ${b})`
-}
+import convert from 'color-convert'
 
 const handler = async () => {
   let editor = vscode.window.activeTextEditor
@@ -53,11 +16,15 @@ const handler = async () => {
 
   let convertedText: string
   if (rgbRegex.test(lineText)) {
-    convertedText = lineText.replace(rgbRegex, (_, r, g, b) =>
-      rgbToHex(+r, +g, +b)
+    convertedText = lineText.replace(
+      rgbRegex,
+      (_, r, g, b) => `#${convert.rgb.hex([r, g, b])}`
     )
   } else if (hexRegex.test(lineText)) {
-    convertedText = lineText.replace(hexRegex, match => hexToRgb(match))
+    convertedText = lineText.replace(
+      hexRegex,
+      match => `rgb(${convert.hex.rgb(match)})`
+    )
   } else {
     return
   }
